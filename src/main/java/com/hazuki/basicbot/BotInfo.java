fd3abdeb11105b38a1ae9442bf69a58663f1af18
Hazuki0511 <manohazuki@gmail.com>
2019-10-22 08:51:24 +0900

    First commit

--- src/main/java/com/hazuki/basicbot/BotInfo.java
+++ src/main/java/com/hazuki/basicbot/BotInfo.java
@@ -4,10 +4,10 @@ import net.dv8tion.jda.api.OnlineStatus;
 
 class BotInfo {
 
-    static final String BOT_TOKEN = "NjMzNTc2MTIyMTk3NzM3NDcy.XaWamA.09P55bvbIJBuhiDfCJdVDtgpcKU";
+    static final String BOT_TOKEN = "NjMzNTc2MTIyMTk3NzM3NDcy.XaqnTQ.EWCswkpca2ki-zQ7pHWPzsE170s";
 
     static final OnlineStatus BOT_STATUS = OnlineStatus.ONLINE;
 
-    static final boolean BOT_AUTO_RECONNECT = false;
+    static final boolean BOT_AUTO_RECONNECT = true;
 
 }
